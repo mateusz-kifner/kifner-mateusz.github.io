09e65d7051172d303e7313e7e25fb0ff8032585c
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2022-08-05 11:51:18 +0200

    add github link to menu

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -4,11 +4,14 @@ import { useState } from "preact/hooks"
 const Nav = () => {
   const [open, setOpen] = useState(false)
   return (
-    <div class="flex justify-between align-middle text-white xl:p-16 p-8">
-      <a href="/" class="font-josefin-sans text-4xl font-bold">
-        <span class="text-white">MK</span>
+    <div class="flex justify-between align-middle text-white xl:p-16 p-8  ">
+      <a
+        href="/"
+        class="font-josefin-sans text-4xl font-bold leading-none hover:text-gray-300"
+      >
+        <span>MK</span>
       </a>
-      <button onClick={() => setOpen((val) => !val)} class="xl:hidden block">
+      {/*<button onClick={() => setOpen((val) => !val)} class="xl:hidden block">
         <img src="/assets/menu.svg" class="h-10  w-10 -translate-y-1" />
       </button>
       <nav
@@ -16,16 +19,37 @@ const Nav = () => {
           open ? "flex" : "hidden"
         }`}
       >
-        <a href="/">
+         <a href="/">
           <span class="text-white hover:text-gray-300">Home</span>
         </a>
         <a href="/projects">
           <span class="text-white hover:text-gray-300">Projekty</span>
-        </a>
-        {/* <a href="/about">
+        </a> 
+         <a href="/about">
           <span class="text-white hover:text-gray-300">O mnie</span>
-        </a> */}
-      </nav>
+        </a> 
+      </nav>*/}
+      <a
+        href="https://github.com/kifner-mateusz"
+        class="flex  hover:text-gray-300 fill-white hover:fill-gray-300 gap-2 font-josefin-sans text-2xl font-bold items-center"
+      >
+        <svg
+          height="32"
+          aria-hidden="true"
+          viewBox="0 0 16 16"
+          version="1.1"
+          width="32"
+          data-view-component="true"
+          class="octicon octicon-mark-github v-align-middle"
+        >
+          <path
+            // fill="white"
+            fill-rule="evenodd"
+            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+          ></path>
+        </svg>
+        <span class="leading-none pt-1">GitHub</span>
+      </a>
     </div>
   )
 }
